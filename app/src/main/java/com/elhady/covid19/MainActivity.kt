8dd108b89caa86c0ea0a6559f58f808e27752a8f
islamelhady <i.elhady44@gmail.com>
2021-08-21 14:42:53 +0200

    hidden and show bottom navigation

--- app/src/main/java/com/elhady/covid19/MainActivity.kt
+++ app/src/main/java/com/elhady/covid19/MainActivity.kt
@@ -1,11 +1,12 @@
 package com.elhady.covid19
 
 import android.os.Bundle
+import android.view.View
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.elhady.covid19.databinding.ActivityMainBinding
@@ -34,10 +35,37 @@ class MainActivity : AppCompatActivity() {
 
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+
+        visibilityNavElements(navController)
     }
 
     override fun onSupportNavigateUp(): Boolean {
         val navController = this.findNavController(R.id.nav_host_fragment_activity_main)
         return navController.navigateUp()
     }
+
+    private fun visibilityNavElements(navController: NavController) {
+
+        //Listen for the change in fragment (navigation) and hide or show drawer or bottom navigation accordingly if required
+        //Modify this according to your need
+        //If you want you can implement logic to deselect(styling) the bottom navigation menu item when drawer item selected using listener
+
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            when (destination.id) {
+                R.id.settingsFragment -> hideBottomNavigation()
+                R.id.faqsFragment -> hideBottomNavigation()
+                else -> showBottomNavigation()
+            }
+        }
+
+    }
+
+    private fun hideBottomNavigation() { //Hide bottom navigation
+        binding.bottomNavView.visibility = View.GONE
+    }
+
+    private fun showBottomNavigation() { //Show bottom navigation
+        binding.bottomNavView.visibility = View.VISIBLE
+    }
+
 }
